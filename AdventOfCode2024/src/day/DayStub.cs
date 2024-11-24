b0bd4dc72b4cc6d9578f489bf14ecb06c41383f4
Matt Hartfield <hartfield@gmail.com>
2024-11-24 11:12:48 -0600

    Added a simple day stub that can be updated as the days go along with common found functionality

---
+++ AdventOfCode2024/src/day/DayStub.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+
+using AdventOfCode2024;
+
+
+public class DayStub : BaseDay
+{
+ 
+    public DayStub()
+    {
+
+    }
+
+    public override async ValueTask<string> Solve_1()
+    {
+        string inputFilePath = InputFilePath(1);
+        if (!File.Exists(inputFilePath))
+        {
+            throw new FileNotFoundException(inputFilePath);
+        }
+
+        int part1 = 0;
+        string[] lines = await File.ReadAllLinesAsync(inputFilePath);
+        Debug.Assert(lines.Length != 0);
+        
+        return new($"Solution 2.1 {part1}");
+    }
+
+
+    public override async ValueTask<string> Solve_2()
+    {
+        string inputFilePath = InputFilePath(2);
+        if (!File.Exists(inputFilePath))
+        {
+            throw new FileNotFoundException(inputFilePath);
+        }
+
+        int part2 = 0;
+        string[] lines = await File.ReadAllLinesAsync(inputFilePath);
+        Debug.Assert(lines.Length != 0);
+        
+        return new($"Solution 2.2 {part2}");
+    }
+}
