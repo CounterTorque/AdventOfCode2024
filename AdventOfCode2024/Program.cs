22eb5afed5aed87766bce4c61652b3bb33b75da7
Matt Hartfield <hartfield@gmail.com>
2024-12-05 14:06:53 -0600

    Day 5 Part 2 - Note the execution took about an hour. This could be optimized.

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -6,6 +6,6 @@ internal class Program
     private static async Task Main(string[] args)
     {
         var factory = new BaseDayFactory<Day05>(input => new Day05(input));
-        await Solver.Solve(factory, Solver.Parts.BOTH);
+        await Solver.Solve(factory, Solver.Parts.Part2);
     }
 }
--- AdventOfCode2024/src/day/Day05.cs
+++ AdventOfCode2024/src/day/Day05.cs
@@ -79,6 +79,8 @@ public class Day05 : BaseDay
         return part1;
     }   
 
+    private Dictionary<int, Dictionary<List<int>, bool>> memoValidateElement = new Dictionary<int, Dictionary<List<int>, bool>>();
+
     private bool ValidateElement(int element, List<int> followingList)
     {
         if (followingList.Count == 0)
@@ -91,7 +93,21 @@ public class Day05 : BaseDay
             return false;
         }
 
-        return followingList.All(x => rules[element].Contains(x));
+        if (!memoValidateElement.ContainsKey(element))
+        {
+            memoValidateElement[element] = new Dictionary<List<int>, bool>();
+        }
+
+        if (!memoValidateElement[element].ContainsKey(followingList))
+        {
+            bool result = followingList.All(x => rules[element].Contains(x));
+            memoValidateElement[element][followingList] = result;
+            return result;
+        }
+        else
+        {
+            return memoValidateElement[element][followingList];
+        }
     }
 
     public override async ValueTask<int> Solve_2()
@@ -116,13 +132,50 @@ public class Day05 : BaseDay
 
                 if (!isValid)
                 {
-                    //TODO: Order this update correctly according to the rules
-                    //Then take the middle element in the update
-                    
+                    for (int i = update.Count - 1; i >= 0; i--)
+                    {
+                        int element = update[i];
+                        List<int> validUpdate = new();
+                        isValid = BuildValidUpdate(element, update.Where(x => x != element).ToList(), ref validUpdate);
+                        if (isValid)
+                        {
+                            part2 += validUpdate[validUpdate.Count / 2];
+                            break;
+                        }
+                    }
                 }
             }
         });
 
         return part2;
+    }
+
+    private bool BuildValidUpdate(int element, List<int> remainingElements, ref List<int> validUpdate)
+    {
+        bool rulesValid = ValidateElement(element, validUpdate);
+        if (!rulesValid)
+        {
+            return false;
+        }
+        
+        validUpdate.Insert(0, element);
+        if (remainingElements.Count == 0)
+        {
+            return true;
+        }
+
+        for (int i = remainingElements.Count - 1; i >= 0; i--)
+        {
+            int nextElement = remainingElements[i];
+            List<int> nextValidUpdate = validUpdate.ToList();
+            bool isValid = BuildValidUpdate(nextElement, remainingElements.Where(x => x != nextElement).ToList(), ref nextValidUpdate);
+            if (isValid)
+            {
+                validUpdate = nextValidUpdate;
+                return true;
+            }
+        }
+      
+        return false;
     }
 }
