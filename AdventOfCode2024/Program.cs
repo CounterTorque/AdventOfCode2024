b8aaa1efe4b6e3eebe845df3bcb5b3e2008ecb8a
Matt Hartfield <hartfield@gmail.com>
2024-12-05 08:01:26 -0600

    Day 5 Part 1 - STUB. Incomplete

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -5,7 +5,7 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        var factory = new BaseDayFactory<Day04>(input => new Day04(input));
+        var factory = new BaseDayFactory<Day05>(input => new Day05(input));
         await Solver.Solve(factory, Solver.Parts.BOTH);
     }
 }
---
+++ AdventOfCode2024/src/day/Day05.cs
@@ -0,0 +1,106 @@
+using System.Diagnostics;
+
+using AdventOfCode2024;
+
+
+public class Day05 : BaseDay
+{
+
+    Dictionary<int, List<int>> rules = new();
+    List<List<int>> updates = new();
+    
+    public Day05(string[]? inputLines = null) : base(inputLines)
+    {
+    }
+
+    public override async ValueTask<int> Solve_1()
+    {
+        int part1 = 0;
+        Debug.Assert(InputLines.Length != 0);
+        bool buildUpdates = false;
+       
+        
+        foreach(string line in InputLines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                buildUpdates = true;
+                continue;
+            }
+
+            if (!buildUpdates)
+            {
+                string[] nums = line.Split('|');
+                Debug.Assert(nums.Length == 2);
+                int left = int.Parse(nums[0].Trim());
+                int right = int.Parse(nums[1].Trim());
+                if (!rules.ContainsKey(left))
+                {
+                    rules.Add(left, new List<int>());
+                }
+
+                rules[left].Add(right);
+                continue;
+            }
+
+            List<int> update = line.Split(',').Select(int.Parse).ToList();
+            updates.Add(update);
+        }
+
+
+        await Task.Run(() => {
+            //For each update
+            //step through from the last element to the first (You may skip the last element since it is always valid)
+            //For each element, check the rules that for the dictionary key of element, all the already seen elements are in its list
+            //Continue until you have validated the entire update. If you get to the end, it is valid
+
+            foreach (List<int> update in updates)
+            {
+                bool isValid = true;
+                for (int i = update.Count - 1; i >= 0; i--)
+                {
+                    int element = update[i];
+                    bool rulesValid = ValidateElement(element, update.Skip(i+1).ToList());
+                    if (!rulesValid)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (isValid)
+                {
+                    //Take the middle element in the update
+                    part1 += update[update.Count / 2];
+                }
+            }
+
+        });
+        
+        return part1;
+    }
+
+    private bool ValidateElement(int element, List<int> followingList)
+    {
+        if (followingList.Count == 0)
+        {
+            return true;
+        }
+
+        //TODO: Missing page numbers in the rules should be ignored. Both here and in the following list.
+        Debug.Assert(rules.ContainsKey(element));
+
+        return followingList.All(x => rules[element].Contains(x));
+    }
+
+    public override async ValueTask<int> Solve_2()
+    {
+        int part2 = 0;
+        Debug.Assert(InputLines.Length != 0);
+        await Task.Run(() => {
+
+        });
+
+        return part2;
+    }
+}
