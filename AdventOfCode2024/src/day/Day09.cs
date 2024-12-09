5c8d87060e53d9c5d78f392655007c615dabf3ba
Matt Hartfield <hartfield@gmail.com>
2024-12-09 09:31:38 -0600

    Day 9 Part 2

--- AdventOfCode2024/src/day/Day09.cs
+++ AdventOfCode2024/src/day/Day09.cs
@@ -6,16 +6,20 @@ using AdventOfCode2024;
 public class Day09 : BaseDay
 {
 
-    List<long> unpackedDisk = new List<long>();
-    
     public Day09(string[]? inputLines = null) : base(inputLines)
     {
+       
+    }
+
+    public override async ValueTask<int> Solve_1()
+    {
+        List<int> unpackedDisk = new List<int>();
         Debug.Assert(InputLines.Length == 1);
         bool isBlock = true;
-        long blockID = 0;
+        int blockID = 0;
         foreach(char c in InputLines[0])
         {
-            long blockSize = long.Parse(c.ToString());
+            int blockSize = int.Parse(c.ToString());
             for(long i = 0; i < blockSize; i++)
             {
                 if (isBlock)
@@ -24,7 +28,7 @@ public class Day09 : BaseDay
                 }
                 else
                 {
-                    unpackedDisk.Add(-blockSize);
+                    unpackedDisk.Add(int.MinValue);
                 }                
             }
             if (isBlock)
@@ -33,68 +37,130 @@ public class Day09 : BaseDay
             }
             isBlock = !isBlock;
         }
-    }
 
-    public override async ValueTask<int> Solve_1()
-    {
         long part1 = 0;
-        await Task.Run(() => {
+        await Task.Run(() =>
+        {
 
             int endIndex = unpackedDisk.Count - 1;
-            int nextEmptyIndex = unpackedDisk.FindIndex(x => x < 0);
-            
+            int nextEmptyIndex = unpackedDisk.FindIndex(x => x == int.MinValue);
+
             while (nextEmptyIndex < endIndex)
             {
-                long endValue = unpackedDisk[endIndex];
-                if (endValue < 0)
+                int endValue = unpackedDisk[endIndex];
+                if (endValue == int.MinValue)
                 {
                     endIndex--;
                     continue;
                 }
 
                 unpackedDisk[nextEmptyIndex] = endValue;
-                unpackedDisk[endIndex] = long.MinValue;
-                nextEmptyIndex = unpackedDisk.FindIndex(nextEmptyIndex + 1, x => x < 0);
+                unpackedDisk[endIndex] = int.MinValue;
+                nextEmptyIndex = unpackedDisk.FindIndex(nextEmptyIndex + 1, x => x == int.MinValue);
             }
 
         });
+        part1 = CheckSum(unpackedDisk);
 
-        //Calculate "checksum"
-        for (int i = 0; i < unpackedDisk.Count; i++)
-        {
-            long value = unpackedDisk[i];
-            if (value < 0)
-            {
-                continue;
-            }
-
-            part1 += (value * i);
-        }
-        
         Console.WriteLine($"Part 1: {part1}");
         return 0;
     }
 
-
-    public override async ValueTask<int> Solve_2()
+    private long CheckSum(List<int> unpackedDisk)
     {
-        long part2 = 0;
-        await Task.Run(() => {
-
-        //TODO: Unpack only if it fits, walking right to left for each file ID
-
-
+        long checkSum = 0;
         //Calculate "checksum"
         for (int i = 0; i < unpackedDisk.Count; i++)
         {
             long value = unpackedDisk[i];
-            if (value < 0)
+            if (value == int.MinValue)
             {
                 continue;
             }
 
-            part2 += (value * i);
+            checkSum += (value * i);
         }
+
+        return checkSum;
+    }
+
+    public override async ValueTask<int> Solve_2()
+    {
+        List<int> unpackedDisk = new List<int>();
+        Debug.Assert(InputLines.Length == 1);
+        bool isBlock = true;
+        int blockID = 0;
+        foreach(char c in InputLines[0])
+        {
+            int blockSize = int.Parse(c.ToString());
+            for(long i = 0; i < blockSize; i++)
+            {
+                if (isBlock)
+                {
+                    unpackedDisk.Add(blockID);
+                }
+                else
+                {
+                    unpackedDisk.Add(int.MinValue);
+                }                
+            }
+            if (isBlock)
+            {
+                blockID++;
+            }
+            isBlock = !isBlock;
+        }
+
+
+        long part2 = 0;
+        await Task.Run(() => {
+
+            int endIndex = unpackedDisk.Count - 1;
+            int FirstEmptyIndex = unpackedDisk.FindIndex(x => x == int.MinValue);
+            
+            while (FirstEmptyIndex < endIndex)
+            {
+                int endValue = unpackedDisk[endIndex];
+                if (endValue == int.MinValue)
+                {
+                    endIndex--;
+                    continue;
+                }
+                int endLength = 0;
+                for (int i = endIndex; i >= FirstEmptyIndex; i--)
+                {
+                    if (unpackedDisk[i] == endValue)
+                    {
+                        endLength++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                //Find the next empty that is large enough.
+                int nextEmptyIndex = FirstEmptyIndex;
+                while (nextEmptyIndex < endIndex)
+                {
+                    int nextPlusEmptyIndex = unpackedDisk.FindIndex(nextEmptyIndex, x => x != int.MinValue);
+                    int emptyLength = nextPlusEmptyIndex - nextEmptyIndex;
+                    if (endLength <= emptyLength)
+                    {
+                        for (int eli = 0; eli < endLength; eli++)
+                        {
+                            unpackedDisk[nextEmptyIndex + eli] = endValue;
+                            unpackedDisk[endIndex - eli] = int.MinValue;
+                        }
+                        break;
+                    }
+                    nextEmptyIndex = unpackedDisk.FindIndex(nextEmptyIndex + emptyLength, x => x == int.MinValue);
+                }
+
+                endIndex -= endLength;
+                FirstEmptyIndex = unpackedDisk.FindIndex(x => x == int.MinValue);
+            }
+
+            part2 = CheckSum(unpackedDisk);
         });
 
         Console.WriteLine($"Part 2: {part2}");
