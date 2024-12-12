d88553e241b0caf5e8d10139a785b83307455a80
Matt Hartfield <hartfield@gmail.com>
2024-12-12 11:08:11 -0600

    Day 12 Part 1

--- AdventOfCode2024/src/day/Day12.cs
+++ AdventOfCode2024/src/day/Day12.cs
@@ -5,7 +5,7 @@ using AdventOfCode2024;
 
 public class Day12 : BaseDay
 {
-    struct Plot
+    class Plot
     {
         public char Type;
         public int FenceSides;
@@ -15,11 +15,11 @@ public class Day12 : BaseDay
     int yMax;
 
     Plot[,] PlotMap;
-    List<List<Plot>> PlotSets = new List<List<Plot>>();
+    List<List<Point>> PlotSets = new List<List<Point>>();
     
     public Day12(string[]? inputLines = null) : base(inputLines)
     {
-        Queue<Point> plotQueue = new Queue<Point>();
+        List<Point> plotCollection = new List<Point>();
         xMax = InputLines[0].Length;
         yMax = InputLines.Length;
         PlotMap = new Plot[xMax, yMax];
@@ -28,25 +28,84 @@ public class Day12 : BaseDay
         {
             for (int x = 0; x < xMax; x++)
             {
-                plotQueue.Enqueue(new Point(x, y));
+                plotCollection.Add(new Point(x, y));
                 char plotType = InputLines[y][x];
                 PlotMap[x, y] = new Plot() { Type = plotType, FenceSides = 0 };
             }
         }
 
-        while (plotQueue.Count > 0)
+        while (plotCollection.Count > 0)
         {
-            Point curPoint = plotQueue.Dequeue();
-            
+            Queue<Point> plotQueue = new Queue<Point>();
+            Point curPoint = plotCollection.Last();
+            plotCollection.Remove(curPoint);
+            plotQueue.Enqueue(curPoint);
+            char currentPlotType = PlotMap[curPoint.X, curPoint.Y].Type;
+            List<Point> plotSet = new List<Point>();
+            PlotSets.Add(plotSet);
+
+            while (plotQueue.Count > 0)
+            {
+                curPoint = plotQueue.Dequeue();
+                Plot curPlot = PlotMap[curPoint.X, curPoint.Y];
+                plotSet.Add(curPoint);
+                Debug.Assert(curPlot.Type == currentPlotType);
+
+                //Look at all the cardinal neighbors. 
+                Point pUp = new Point(curPoint.X, curPoint.Y - 1);
+                Point pDown = new Point(curPoint.X, curPoint.Y + 1);
+                Point pLeft = new Point(curPoint.X - 1, curPoint.Y);
+                Point pRight = new Point(curPoint.X + 1, curPoint.Y);
+
+                if (pUp.Y >= 0 && pUp.X >= 0 && pUp.X < xMax && pUp.Y < yMax && PlotMap[pUp.X, pUp.Y].Type == currentPlotType)
+                {
+                    if (plotCollection.Remove(pUp))
+                    {
+                        plotQueue.Enqueue(pUp);
+                    }
+                }    
+                else
+                {
+                    curPlot.FenceSides++;
+                }
+
+                if (pDown.Y >= 0 && pDown.X >= 0 && pDown.X < xMax && pDown.Y < yMax && PlotMap[pDown.X, pDown.Y].Type == currentPlotType)
+                {
+                    if (plotCollection.Remove(pDown))
+                    {
+                        plotQueue.Enqueue(pDown);
+                    };
+                }
+                else
+                {
+                    curPlot.FenceSides++;
+                }
+
+                if (pLeft.Y >= 0 && pLeft.X >= 0 && pLeft.X < xMax && pLeft.Y < yMax && PlotMap[pLeft.X, pLeft.Y].Type == currentPlotType)
+                {
+                    if (plotCollection.Remove(pLeft))
+                    {
+                        plotQueue.Enqueue(pLeft);
+                    }
+                }
+                else
+                {
+                    curPlot.FenceSides++;
+                }
+
+                if (pRight.Y >= 0 && pRight.X >= 0 && pRight.X < xMax && pRight.Y < yMax && PlotMap[pRight.X, pRight.Y].Type == currentPlotType)
+                {
+                    if (plotCollection.Remove(pRight))
+                    {
+                        plotQueue.Enqueue(pRight);
+                    }
+                }
+                else
+                {
+                    curPlot.FenceSides++;
+                }
+            }  
         }
-
-        //TODO: Instead of walking the queue, we need to look at all the neighbors of the current point. 
-        // If they are not visited, add those to a seperate queue for this plot TYPE. 
-        // We need to store the Plots in a dictionary based on the plot type. 
-        //However, we need more than just the type, because we can have duplicate, but unique plot types that are disconnected. 
-
-
-
     }
 
     public override async ValueTask<int> Solve_1()
@@ -56,12 +115,13 @@ public class Day12 : BaseDay
         Debug.Assert(InputLines.Length != 0);
         await Task.Run(() => {
 
-            foreach (List<Plot> plotset in PlotSets)
+            foreach (List<Point> plotset in PlotSets)
             {
                 int area = plotset.Count;
                 int pererimeter = 0;
-                foreach (Plot plot in plotset)
+                foreach (Point plotPoint in plotset)
                 {
+                    Plot plot = PlotMap[plotPoint.X, plotPoint.Y];
                     pererimeter += plot.FenceSides;
                 }
                 part1 += area * pererimeter;
