ba634663be0194eba037fae87be644cac524bd9b
Matt Hartfield <hartfield@gmail.com>
2024-12-17 12:16:48 -0600

    Day 17 Part 2 - Will take 210 days max. Need to figure out how to walk the instruction tree backwards maybe."

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -6,6 +6,6 @@ internal class Program
     private static async Task Main(string[] args)
     {
         var factory = new BaseDayFactory<Day17>(input => new Day17(input));
-        await Solver.Solve(factory, Solver.Parts.Part1);
+        await Solver.Solve(factory, Solver.Parts.Part2);
     }
 }
--- AdventOfCode2024/src/day/Day17.cs
+++ AdventOfCode2024/src/day/Day17.cs
@@ -5,7 +5,6 @@ using AdventOfCode2024;
 
 public class Day17 : BaseDay
 {
-
     enum InstructionType
     {
         Iadv = 0,
@@ -18,9 +17,9 @@ public class Day17 : BaseDay
         Icdv = 7
     }
 
-    int rA = 47792830;
-    int rB = 0;
-    int rC = 0;
+    long rA = 47792830;
+    long rB = 0;
+    long rC = 0;
 
     struct Instruction
     {
@@ -35,7 +34,7 @@ public class Day17 : BaseDay
     {
         Debug.Assert(InputLines.Length == 1);
         char[] line = InputLines[0].ToCharArray();
-        for (int i = 0; i < line.Length; i=i+4)
+        for (int i = 0; i < line.Length; i = i + 4)
         {
             int pI = int.Parse(line[i].ToString());
             int pV = int.Parse(line[i + 2].ToString());
@@ -51,105 +50,155 @@ public class Day17 : BaseDay
     public override async ValueTask<int> Solve_1()
     {
         int part1 = 0;
-        int instructionPointer = 0;
+        string output = "";
 
         await Task.Run(() =>
         {
-            while (instructionPointer < Instructions.Count)
-            {
-                Instruction instruction = Instructions[instructionPointer];
-                
-                switch (instruction.Type)
-                {
-                    case InstructionType.Iadv:
-                        {
-                            int instVal = GetInstructionValue(instruction);
-                            double val = rA / Math.Pow(2, instVal);
-                            rA = (int)val;
-                            instructionPointer++;
-                        }
-                        break;
-                    case InstructionType.Ibxl:
-                        {
-                            int val = rB ^ instruction.Value;
-                            rB = val;
-                            instructionPointer++;
-                        }
-                        break;
-                    case InstructionType.Ibst:
-                        {
-                            int instVal = GetInstructionValue(instruction);
-                            rB = instVal % 8;
-                            instructionPointer++;
-                        }
-                        break;
-                    case InstructionType.Ijnz:
-                        {
-                            if (rA != 0)
-                            {
-                                instructionPointer = instruction.Value;
-                                //NOTE: This may cause a jump that mixes the values and ops. Check for this if it's not working.
-                            }
-                            else
-                            {
-                                instructionPointer++;
-                            }
-                        }
-                        break;
-                    case InstructionType.Ibxc:
-                        {
-                            rB = rB ^ rC;
-                            instructionPointer++;
-                        }
-                        break;
-                    case InstructionType.Iout:
-                        {
-                            int instVal = GetInstructionValue(instruction);
-                            int val = instVal % 8;
-                            Console.Write($"{val},");
-                            instructionPointer++;
-                        }
-                        break;
-                    case InstructionType.Ibdv:
-                        {
-                            int instVal = GetInstructionValue(instruction);
-                            double val = rA / Math.Pow(2, instVal);
-                            rB = (int)val;
-                            instructionPointer++;
-                        }
-                        break;
-                    case InstructionType.Icdv:
-                        {
-                            int instVal = GetInstructionValue(instruction);
-                            double val = rA / Math.Pow(2, instVal);
-                            rC = (int)val;
-                            instructionPointer++;
-                        }
-                        break;
-                    default:
-                        Debug.Assert(false);
-                        break;
-                }
-            }
+            //SAMPLE 1
+            output = ExecuteInstructions(2024, 0, 0);
+
+            //PUZZLE INPUT
+            //string output = ExecuteInstructions(47792830, 0, 0);
 
         });
 
+        Console.WriteLine(output);
         return part1;
     }
 
     public override async ValueTask<int> Solve_2()
     {
-        int part2 = 0;
-        Debug.Assert(InputLines.Length != 0);
+        //string target = "0,3,5,4,3,0,"; //TEST VALUE
+        string target = "2,4,1,5,7,5,1,6,4,3,5,5,0,3,3,0,"; //PUZZLE INPUT
+        List<long> targetSeq = target.Split(',').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => long.Parse(x)).ToList();
+            
+
         await Task.Run(() =>
         {
+            long rACheck = 100001900000000; //Last Known Tested
+          //long rACheck = 281474976710656; //Has to be less than this, right?? 2^48 (3bits*16insts)
+            while (true)
+            {
+                string output = ExecuteInstructions(rACheck, 0, 0, targetSeq);
+                if (rACheck % 10000000 == 0)
+                {
+                    Console.WriteLine($"{rACheck}: {output}");
+                }
+                
+                if (output.Equals(target))
+                {
+                    Console.WriteLine($"FINAL: {rACheck}: {output}");
+                    break;
+                }
+                
+                rACheck++;                
+            }
+            
 
         });
 
-        return part2;
+        //100000000000000 TOLOW
+        return 0;
     }
 
-    int GetInstructionValue(Instruction instruction)
+    private string ExecuteInstructions(long rASet, long rBSet, long rCSet, List<long> targetSeq = null)
+    {
+        string output = "";
+        int outputIndex = 0;
+
+        rA = rASet;
+        rB = rBSet;
+        rC = rCSet;
+        int instructionPointer = 0;
+        while (instructionPointer < Instructions.Count)
+        {
+            Instruction instruction = Instructions[instructionPointer];
+
+            switch (instruction.Type)
+            {
+                case InstructionType.Iadv:
+                    {
+                        long instVal = GetInstructionValue(instruction);
+                        double val = rA / Math.Pow(2, instVal);
+                        rA = (long)val;
+                        instructionPointer++;
+                    }
+                    break;
+                case InstructionType.Ibxl:
+                    {
+                        long val = rB ^ instruction.Value;
+                        rB = val;
+                        instructionPointer++;
+                    }
+                    break;
+                case InstructionType.Ibst:
+                    {
+                        long instVal = GetInstructionValue(instruction);
+                        rB = instVal % 8;
+                        instructionPointer++;
+                    }
+                    break;
+                case InstructionType.Ijnz:
+                    {
+                        if (rA != 0)
+                        {
+                            instructionPointer = instruction.Value;
+                            //NOTE: This may cause a jump that mixes the values and ops. Check for this if it's not working.
+                        }
+                        else
+                        {
+                            instructionPointer++;
+                        }
+                    }
+                    break;
+                case InstructionType.Ibxc:
+                    {
+                        rB = rB ^ rC;
+                        instructionPointer++;
+                    }
+                    break;
+                case InstructionType.Iout:
+                    {
+                        long instVal = GetInstructionValue(instruction);
+                        long val = instVal % 8;
+                        if (targetSeq != null)
+                        {
+                            if (val != targetSeq[outputIndex])
+                            {
+                                return output;
+                            }
+                            outputIndex++;
+                        }
+                        output += $"{val},";
+                        instructionPointer++;
+                    }
+                    break;
+                case InstructionType.Ibdv:
+                    {
+                        long instVal = GetInstructionValue(instruction);
+                        double val = rA / Math.Pow(2, instVal);
+                        rB = (long)val;
+                        instructionPointer++;
+                    }
+                    break;
+                case InstructionType.Icdv:
+                    {
+                        long instVal = GetInstructionValue(instruction);
+                        double val = rA / Math.Pow(2, instVal);
+                        rC = (long)val;
+                        instructionPointer++;
+                    }
+                    break;
+                default:
+                    Debug.Assert(false);
+                    break;
+            }
+        }
+
+        return output;
+    }
+
+    long GetInstructionValue(Instruction instruction)
     {
         if (instruction.Value <= 3)
             return instruction.Value;
